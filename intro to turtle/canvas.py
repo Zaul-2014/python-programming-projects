23235afad6087ffdda1b33ae6b59b8b23432ebc1
zaul-2014 <zeenathsaee09@gmail.com>
2025-11-28 10:40:03 +0500

    Add files via upload
    its inside intro to turtle

---
+++ intro to turtle/canvas.py
@@ -0,0 +1,23 @@
+import turtle, time
+
+screen = turtle.Screen()
+screen.screensize(500, 500)
+screen.bgcolor('light blue')
+
+t = turtle.Turtle()
+
+
+
+
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+
+time.sleep(3)
+
+turtle.done()
